8d3b30bb9a7bbb418faaff02c181420b1e423673
HickOSDio <99850263+HickOSDio@users.noreply.github.com>
2022-07-29 11:13:28 -0300

    Add files via upload

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package com.hickos;
+
+public class ParametrosInvalidosException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+}
